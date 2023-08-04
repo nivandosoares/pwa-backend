1758b423fbaeff66d9384b426311946ad28cba78
Nivando <sidvandoni@gmail.com>
2023-08-04 15:20:17 -0300

    fixed launch scripts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const env = require("dotenv").config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -38,7 +38,7 @@ app.get("/", async (req, res) => {
     res.json(classrooms);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "ppppppppppppp server error" });
+    res.status(500).json({ message: "server error" });
   }
 });
 
